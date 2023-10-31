a0bdd0bd327c37cac27658db2410a2e13e73998b
ericaskari <ericaskari@gmail.com>
2023-10-31 16:59:41 +0200

    llm training set

--- llm/main.py
+++ llm/main.py
@@ -1,11 +1,17 @@
 import os
 from icecream import ic
 from ultralytics import YOLO
+import cv2
 
+# Load the image
+image_path = "images/two.png"
 
 model = YOLO("models/best-epoch-1.pt")
 
-results = model.predict("images/g4.png")
+results = model.predict(image_path)
+
+image = cv2.imread(image_path)
+
 
 ic(len(results))
 for result in results:
@@ -16,10 +22,26 @@ for result in results:
         ic(len(box.cls))
         for i, class_conf in enumerate(box.cls):
             class_id = result.names[class_conf.item()]
-            cords = box.xyxy[i].tolist()
-            cords = [round(x) for x in cords]
+            coordinates = box.xyxy[i].tolist()
+            coordinates = [round(x) for x in coordinates]
+
             conf = round(box.conf[i].item(), 2)
             print("Object type:", class_id)
-            print("Coordinates:", cords)
+            print("Coordinates:", coordinates)
             print("Probability:", conf)
             print("---")
+
+            # Extract coordinates
+            x_min, y_min, x_max, y_max = [int(coord) for coord in coordinates]
+            # Draw a red rectangle around the detected object
+            cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 0, 255), 2)
+            # Display class name
+            cv2.putText(image, class_id, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+# Save the image with red borders
+output_image_path = "images/output.png"
+cv2.imwrite(output_image_path, image)
+# Display the image (optional)
+cv2.imshow("Image with Red Borders", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
