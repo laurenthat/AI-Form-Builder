46e72ff08add8f4e67718aa6b0b80905230f85d2
ericaskari <ericaskari@gmail.com>
2023-11-01 11:13:33 +0200

    moved llm to another repository

--- llm/train.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import torch
-from ultralytics import YOLO
-
-if __name__ == "__main__":
-    print(torch.cuda.is_available())
-    model = YOLO("yolov8n.pt")
-    model.train(data=os.path.join(os.getcwd(), 'dataset', "data.yaml"),epochs=5,model="ai-form-builder.pt", imgsz=224)
