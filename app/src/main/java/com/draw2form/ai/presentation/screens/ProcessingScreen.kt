9836f2685d0c0e9f990311e3fd3cde9e3b09f239
shaykandagor <shaynekandagor@gmail.com>
2023-11-16 16:26:32 +0200

    Add form loading screen

---
+++ app/src/main/java/com/draw2form/ai/presentation/screens/ProcessingScreen.kt
@@ -0,0 +1,152 @@
+package com.draw2form.ai.presentation.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.draw2form.ai.presentation.ui.theme.LinkUpTheme
+import com.draw2form.ai.R
+
+
+data class ImageProcessState(
+    val objectRecognition: String,
+    val textRecognition: String,
+    val formGeneration: String,
+    )
+
+@Composable
+fun ImageProcessStatus(state: String, label: String) {
+
+    val successComposition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.animation_success))
+    val loadingComposition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.animation_loading))
+    val errorComposition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.animation_error))
+
+
+    when (state) {
+        "loading" ->
+            Row(
+                modifier = Modifier
+                    .padding(20.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                LottieAnimation(
+                    composition = loadingComposition,
+                    iterations = LottieConstants.IterateForever,
+                    modifier = Modifier.size(50.dp)
+
+                    )
+                Text(text = label, textAlign = TextAlign.Center, fontSize = 16.sp)
+
+            }
+
+        "success" ->
+            Row(
+                modifier = Modifier
+                    .padding(20.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                LottieAnimation(
+                    composition = successComposition,
+                    iterations =  1,
+                    modifier = Modifier.size(50.dp)
+                )
+                Text(text = label, textAlign = TextAlign.Center, fontSize = 16.sp)
+
+
+            }
+
+        "error" ->
+            Row(
+                modifier = Modifier
+                    .padding(20.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                LottieAnimation(
+                    composition = errorComposition,
+                    iterations = 1,
+                    modifier = Modifier.size(50.dp)
+                )
+                Text(text = label, textAlign = TextAlign.Center, fontSize = 16.sp)
+
+            }
+
+    }
+
+}
+
+@Composable
+fun ViewInterface(onClick: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .padding(30.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { onClick() }) {
+            Text(text = "View Form")
+
+        }
+    }
+}
+
+@Composable
+fun ProcessingScreen() {
+
+    LazyColumn(
+        modifier = Modifier
+            .padding(horizontal = 20.dp, vertical = 150.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        item {
+            Column(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                val apiResponse = ImageProcessState("error", "loading", "success")
+
+                ImageProcessStatus(apiResponse.objectRecognition, "Object Recognition")
+                ImageProcessStatus(apiResponse.textRecognition, "Text Recognition")
+                ImageProcessStatus(apiResponse.formGeneration, "Form Generation")
+
+                ViewInterface(onClick = {})
+            }
+
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ProcessingScreenPreview() {
+    LinkUpTheme {
+        ProcessingScreen()
+    }
+}
+
