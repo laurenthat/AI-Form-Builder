4c0ba3e3bc65ec2da8eaea9e7252198814a7ba5e
shaykandagor <shaynekandagor@gmail.com>
2023-11-24 10:49:03 +0200

    Add search feature on forms list screen

--- app/src/main/java/com/draw2form/ai/presentation/screens/FormsListScreen.kt
+++ app/src/main/java/com/draw2form/ai/presentation/screens/FormsListScreen.kt
@@ -1,36 +1,180 @@
 package com.draw2form.ai.presentation.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.draw2form.ai.R
 import com.draw2form.ai.api.ApiForm
 import com.draw2form.ai.application.AppViewModelProvider
 import com.draw2form.ai.presentation.ui.theme.LinkUpTheme
 import com.draw2form.ai.user.UserViewModel
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FormsListScreen(userViewModel: UserViewModel = viewModel(factory = AppViewModelProvider.Factory)) {
+fun FormsListScreenTopBar(
+    scrollBehavior: TopAppBarScrollBehavior,
+) {
+    MediumTopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Text(
+                stringResource(R.string.my_forms),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                fontSize = 20.sp
+            )
+        },
+        scrollBehavior = scrollBehavior
+    )
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormsListScreen(
+    modifier: Modifier = Modifier,
+    userViewModel: UserViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
 
     val userForms = userViewModel.apiUserForms.collectAsState(emptyList())
     val forms = userForms.value
 
-    CategorizedLazyColumn(forms = forms)
-
     LaunchedEffect(true) {
         userViewModel.getUserForms()
+    }
+
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
+    var searchQuery by remember { mutableStateOf("") }
+
+    var showClearIcon by rememberSaveable { mutableStateOf(false) }
+
+    if (searchQuery.isEmpty()) {
+        showClearIcon = false
+    } else if (searchQuery.isNotEmpty()) {
+        showClearIcon = true
+    }
+
+    val filteredForms = if (searchQuery.isEmpty()) {
+        forms
+    } else {
+        forms.filter { form ->
+            form.name.contains(searchQuery, ignoreCase = true)
+        }
+    }
+
+    Scaffold(
+        topBar = {
+            FormsListScreenTopBar(scrollBehavior = scrollBehavior)
+        },
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+        ) {
+            // Search Bar
+            OutlinedTextField(
+                value = searchQuery,
+                onValueChange = { newValue ->
+                    searchQuery = newValue
+                },
+                singleLine = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .background(
+                        color = MaterialTheme.colorScheme.primaryContainer,
+                        shape = RoundedCornerShape(16.dp)
+                    ),
+                placeholder = {
+                    Text(
+                        text = "Search Forms",
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                },
+                trailingIcon = {
+                    if (showClearIcon) {
+                        IconButton(onClick = { searchQuery = "" }) {
+                            Icon(
+                                imageVector = Icons.Rounded.Clear,
+                                tint = MaterialTheme.colorScheme.onBackground,
+                                contentDescription = "Clear Icon"
+                            )
+                        }
+                    }
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
+            )
+            LazyColumn(
+                modifier
+                    .padding(start = 20.dp, end = 20.dp)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(1.dp),
+            ) {
+                items(filteredForms) { form ->
+                    FormItem(form)
+                }
+            }
+        }
     }
 }
 
@@ -41,6 +185,10 @@ fun FormItem(form: ApiForm) {
         modifier = Modifier
             .padding(10.dp)
             .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colorScheme.primaryContainer,
+                shape = RoundedCornerShape(16.dp)
+            )
     ) {
         Text(
             text = form.name,
@@ -63,23 +211,6 @@ fun FormItem(form: ApiForm) {
         )
     }
 
-}
-
-
-@Composable
-private fun CategorizedLazyColumn(
-    forms: List<ApiForm>,
-    modifier: Modifier = Modifier
-) {
-    val sortedForms = forms.sortedBy { it.name }
-
-    LazyColumn(
-        modifier = modifier
-    ) {
-        items(sortedForms) { form ->
-            FormItem(form)
-        }
-    }
 }
 
 @Preview(showBackground = true)
