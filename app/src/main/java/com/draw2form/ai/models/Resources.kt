5bb56588bfe52c862630000fd928b863f890df3e
ericaskari <ericaskari@gmail.com>
2023-12-05 17:30:45 +0200

    Ordering fix

---
+++ app/src/main/java/com/draw2form/ai/models/Resources.kt
@@ -0,0 +1,10 @@
+package com.draw2form.ai.models
+
+enum class Resources(val value: String) {
+    FormButton("button"),
+    FormImage("image"),
+    FormLabel("label"),
+    FormCheckbox("checkbox"),
+    FormTextField("text-field"),
+    FormToggleSwitch("toggle-switch"),
+}
