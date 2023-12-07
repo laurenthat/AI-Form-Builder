9f910211f957aad2f4f987a16dffff88c6bb9f58
frozenfi <iceburns1989@gmail.com>
2023-12-07 22:56:33 +0200

    link share feature added, ui bug fixed

--- app/src/main/java/com/draw2form/ai/presentation/screens/InstructionsScreen.kt
+++ app/src/main/java/com/draw2form/ai/presentation/screens/InstructionsScreen.kt
@@ -28,11 +28,13 @@ import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -44,8 +46,7 @@ import com.draw2form.ai.presentation.ui.theme.BottomBarColor
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun InstructionScreenTopBar(onBackClick: () -> Unit) {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+fun InstructionScreenTopBar(scrollBehavior: TopAppBarScrollBehavior, onBackClick: () -> Unit) {
 
     MediumTopAppBar(
         colors = TopAppBarDefaults.topAppBarColors(
@@ -74,12 +75,18 @@ fun InstructionScreenTopBar(onBackClick: () -> Unit) {
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InstructionsScreen(onBackClick: () -> Unit) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
     Scaffold(
         topBar = {
-            InstructionScreenTopBar(onBackClick = onBackClick)
-        }
+            InstructionScreenTopBar(scrollBehavior, onBackClick = onBackClick)
+        },
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
     ) { padding ->
         Column(
             modifier = Modifier
@@ -170,6 +177,7 @@ fun InstructionsScreen(onBackClick: () -> Unit) {
                     }
                 }
             }
+
         }
     }
 }
--- app/src/main/java/com/draw2form/ai/presentation/screens/ShareFormScreen.kt
+++ app/src/main/java/com/draw2form/ai/presentation/screens/ShareFormScreen.kt
@@ -1,5 +1,9 @@
 package com.draw2form.ai.presentation.screens
 
+import android.content.Intent
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,8 +13,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -19,16 +26,18 @@ import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.draw2form.ai.R
 import com.draw2form.ai.presentation.components.rememberQrBitmapPainter
@@ -42,32 +51,48 @@ fun ShareFormScreen(shareId: String, onBackClick: () -> Unit) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GenerateQRCode(
     data: String, onBackClick: () -> Unit
 ) {
     val painter = rememberQrBitmapPainter(content = data)
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie_qr))
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
+    val context = LocalContext.current
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) {
+    }
+
 
     Scaffold(
         topBar = {
-            GenerateQRCodeTopBar {
+            GenerateQRCodeTopBar(
+                scrollBehavior = scrollBehavior
+
+            ) {
                 onBackClick()
             }
         },
         modifier = Modifier
             .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
     ) { padding ->
         Column(
             modifier = Modifier
-                .padding(padding)
                 .fillMaxSize()
-                .padding(16.dp),
+                .padding(padding)
+                .verticalScroll(rememberScrollState()),
+
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-            Spacer(modifier = Modifier.height((-2).dp))
+            //Spacer(modifier = Modifier.height((-2).dp))
+
             Text(
                 text = "Scan QR to fill the Form",
                 style = MaterialTheme.typography.labelLarge,
@@ -84,21 +109,37 @@ fun GenerateQRCode(
                 contentDescription = null,
                 modifier = Modifier.size(200.dp)
             )
-            Spacer(modifier = Modifier.height(12.dp))
+            Spacer(modifier = Modifier.height(22.dp))
 
-            LottieAnimation(
-                composition = composition,
-                iterations = LottieConstants.IterateForever,
-                modifier = Modifier.size(250.dp)
-            )
+            Button(onClick = {
+                shareViaLink(data, context, launcher)
+            }) {
+                Text(text = "Share your form link")
+            }
+
+
         }
     }
+}
+
+fun shareViaLink(
+    id: String,
+    context: android.content.Context,
+    launcher: ActivityResultLauncher<Intent>
+) {
+    val sendIntent = Intent(Intent.ACTION_SEND)
+        .setType("text/plain")
+        .putExtra(Intent.EXTRA_TEXT, id)
+
+    val shareIntent = Intent.createChooser(sendIntent, null)
+    launcher.launch(shareIntent)
 }
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GenerateQRCodeTopBar(
+    scrollBehavior: TopAppBarScrollBehavior,
     onBackClick: () -> Unit
 ) {
     MediumTopAppBar(
@@ -108,7 +149,7 @@ fun GenerateQRCodeTopBar(
         ),
         title = {
             Text(
-                text = "Share Forms via QR Code",
+                text = "Share Form",
                 style = MaterialTheme.typography.labelLarge,
                 fontSize = 20.sp
             )
@@ -124,5 +165,6 @@ fun GenerateQRCodeTopBar(
                 )
             }
         },
+        scrollBehavior = scrollBehavior
     )
 }
